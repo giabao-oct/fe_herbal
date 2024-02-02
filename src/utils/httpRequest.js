3a584b494358d126d7ba08e990ff6e1b14a21e3c
giabao.oct <80464910+giabao-oct@users.noreply.github.com>
2024-02-02 11:34:49 +0700

    Update httpRequest.js

--- src/utils/httpRequest.js
+++ src/utils/httpRequest.js
@@ -1,3 +1,3 @@
-// const baseURL = "http://127.0.0.1:8888/";
-const baseURL = "https://sensibly-lasting-humpback.ngrok-free.app/";
+const baseURL = "http://127.0.0.1:8888/";
+// const baseURL = "https://sensibly-lasting-humpback.ngrok-free.app/";
 export default baseURL;
